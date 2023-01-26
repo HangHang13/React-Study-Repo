c676314c4402d32a1e4d8b4353236efd4d518902
HangHang13 <dhkdwlsgod@naver.com>
2023-01-26 15:58:18 +0900

    feat : 이메일 중복확인 검증

--- 실습/todo-app/src/App.js
+++ 실습/todo-app/src/App.js
@@ -4,13 +4,36 @@ import "./App.css";
 import TodoTemplate from "./todo/TodoTemplate";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import Join from "./components/user/Join";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Snowfall from 'react-snowfall'
+
+
+
+
+
 function App() {
   return (
     <>
       {/* <Hello></Hello> */}
       {/* <ItemMain></ItemMain> */}
+      {/* <Snowfall
+  // Changes the snowflake color
+  color="blue"
+  // Applied to the canvas element
+  style={{ background: '#fff', position: 'fixed',
+  width: '100vw',
+  height: '100vh' }}
+  // Controls the number of snowflakes that are created (default 150)
+  snowflakeCount={250}
+  wind={[-0.5, 2.0]}
+  speed={[1.0, 3.0]
+  }
+/> */}
       <Header/>
-      <TodoTemplate/>
+      
+      <Join/>
+      {/* <TodoTemplate/> */}
       <Footer/>
     </>
   );
--- 실습/todo-app/src/todo/TodoTemplate.js
+++ 실습/todo-app/src/todo/TodoTemplate.js
@@ -5,10 +5,18 @@ import TodoMain from "./TodoMain";
 
 //css로딩
 import "./css/TodoCss.css";
+import { Spinner } from "reactstrap";
+import { BASE_URL, TODO} from "../config/host-config";
 
 const TodoTemplate = () => {
 
-  const API_BASE_URL = "http://localhost:8080/api/todos/";
+
+  const API_BASE_URL = BASE_URL + TODO;
+  
+
+
+  //로딩중 상태
+  const [loading, setLoading] = useState(true);
 
   const [todos, setTodos] = useState([]);
   //할일 등록 서버요청
@@ -51,18 +59,53 @@ const TodoTemplate = () => {
 
   useEffect(() => {
     fetch(API_BASE_URL)
-      .then((res) => res.json())
+      .then((res) => 
+        {
+        if (res.status===403){
+          alert("로그인이 필요한 서비스입니다.")
+          return;
+        }else if (res.status===500){
+          alert("서버에러입니다.")
+          return;
+        }
+      
+        return res.json();
+        })
       .then((res) => {
         setTodos(res.todos);
+        
+        //로딩완료처리
+        setLoading(false);
       });
     // dispatch(todoListGet)
   }, []);
-  return (
+  //로딩중일때 보여줄 태그
+  const loadingPage = (
+    <div className="loading">
+      <Spinner color="success">
+        Loading....
+      </Spinner>
+    </div>
+
+
+  );
+
+
+  //로딩완료시 보여줄 태그
+  const viewPage = (
     <div className="todo-template">
       <TodoHeader todoList={todos} />
       <TodoMain todoList={todos} todoDelete={deleteTodo} updateCheck={updateCheck}/>
       <TodoInput add={addTodo} />
     </div>
+  );
+
+
+  return (
+    <>
+      {loading ? loadingPage : viewPage}
+    </>
+    
   );
 };
 
